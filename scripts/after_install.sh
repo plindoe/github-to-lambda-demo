38beeb2118d557a7bda292420d01ab6e7786da89
PhilLindoe <phillindoe@lifung.com>
2023-08-24 13:06:12 +0100

    after install updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Deploying new Lambda code..."
-aws lambda update-function-code --function-name github-lambda-demo2 --zip-file fileb://deployment_package.zip
+aws lambda update-function-code --function-name github-lambda-demo --zip-file fileb://deployment_package.zip
